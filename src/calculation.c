5f87a631eab30654771b3eb8a64d88ad805c9ae8
antonafonix <antonafonin567@gmail.com>
2026-09-10 22:46:21 +0100

    fixed logic of adding operators to output queue

--- src/calculation.c
+++ src/calculation.c
@@ -24,24 +24,29 @@ int calculate(Token *tokens, int size) {
 
     int index = 0;
     while (index < size) {
-        int value = tokens[index].value[0];
+        char value = tokens[index].value[0];
         TokenType value_type = tokens[index].type;
 
         if (TOKEN_NUMBER == value_type) {
             enqueue(&output_queue, value);
         } else if (TOKEN_OPERATOR == value_type) {
-            if (is_stack_empty(&operators_stack)) {
-                push(&operators_stack, value);
-            } else {
-                while (operators_stack.top != -1 && get_priority(peek_stack(&operators_stack)) > get_priority(value)) {
-                    char op2 = pop(&operators_stack);
-                    enqueue(&output_queue, op2);   
-                }
-                push(&operators_stack, value);
+
+            while (operators_stack.top != -1 &&
+                   get_priority(peek_stack(&operators_stack)) >= get_priority(value)) {
+                char op2 = pop(&operators_stack);
+                enqueue(&output_queue, op2);
+                printf("added %c to the output queue\n", op2);
             }
+
+            push(&operators_stack, value);
         }
 
         index++;
+    }
+
+    while (operators_stack.top != -1) {
+        char op2 = pop(&operators_stack);
+        enqueue(&output_queue, op2);
     }
 
     printQueue(&output_queue);
